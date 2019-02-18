e12dcba0eafe4b639351fc1900d9f381c162cd31
Omoefe Dukuye <omoefe.dukuye@gmail.com>
2019-02-18 13:08:04 +0100

    Scroll to buttom if user is close

--- public/js/index.js
+++ public/js/index.js
@@ -2,6 +2,22 @@
 
 const socket = io();
 
+const scrollToButtom = () => {
+  const messages = jQuery('#messages');
+  const newMessage = messages.children('li:last-child');
+
+  const clientHeight = messages.prop('clientHeight');
+  const scrollTop = messages.prop('scrollTop');
+  const scrollHeight = messages.prop('scrollHeight');
+
+  const newMessageHeight = newMessage.innerHeight();
+  const lastMessageHeight = newMessage.prev().innerHeight();
+
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  }
+};
+
 socket.on('connect', () => {
   console.log('Connected to server');
 
@@ -10,6 +26,7 @@ socket.on('connect', () => {
     const template = jQuery('#message-template').html();
     const html = Mustache.render(template, { from, text, createdAt });
     jQuery('#messages').append(html);
+    scrollToButtom();
   });
 
   socket.on('newLocationMessage', ({ from, url, createdAt }) => {
@@ -17,6 +34,7 @@ socket.on('connect', () => {
     const template = jQuery('#location-message-template').html();
     const html = Mustache.render(template, { from, url, createdAt });
     jQuery('#messages').append(html);
+    scrollToButtom();
   });
 
   jQuery('#message-form').on('submit', (e) => {
